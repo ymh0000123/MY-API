29ef0a4f32f52660e6cce78f45392c8a9173c7c7
ymh0000123 <ymh0000123@163.com>
2024-06-25 14:30:18 +0800

    fix

--- functions/api/ip.js
+++ functions/api/ip.js
@@ -1,1 +1,6 @@
-
+export async function onRequest(context) {
+    const clientIP = context.request.headers.get('cf-connecting-ip') || 'IP Not Found';
+    return new Response(clientIP, {
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
