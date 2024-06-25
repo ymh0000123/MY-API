eaf951d9579c75f0ad7a5e6d2abf484d58dcc5d8
ymh0000123 <ymh0000123@163.com>
2024-06-25 14:20:12 +0800

    fix

--- functions/api/ip.js
+++ functions/api/ip.js
@@ -1,29 +1,8 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  // 获取请求的IP地址
-  const ip = request.headers.get('CF-Connecting-IP')
-
-  // 获取请求的浏览器信息
-  const userAgent = request.headers.get('User-Agent')
-
-  // 获取请求的地区信息
-  const cf = request.cf
-  const country = cf?.country // 获取国家信息
-  const city = cf?.city // 获取城市信息
-
-  // 构造响应数据
-  const response = {
-    ip: ip,
-    userAgent: userAgent,
-    country: country,
-    city: city
-  }
-
-  // 返回JSON格式的响应
-  return new Response(JSON.stringify(response), {
-    headers: { 'Content-Type': 'application/json' },
-  })
+export async function handler(event, context) {
+  const ip = event.request.headers.get('cf-connecting-ip'); // Cloudflare specific header
+  return new Response(ip || 'Unable to determine IP address', {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
 }
