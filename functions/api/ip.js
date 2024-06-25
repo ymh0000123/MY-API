8ec6cd8fc00cfbdd85ef7cba1eac0d9a27c23059
ymh0000123 <ymh0000123@163.com>
2024-06-25 14:23:08 +0800

    空

--- functions/api/ip.js
+++ functions/api/ip.js
@@ -1,11 +1,1 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
 
-async function handleRequest(request) {
-  const ip = request.headers.get('CF-Connecting-IP') || request.headers.get('X-Forwarded-For') || request.headers.get('x-real-ip') || request.headers.get('Remote_Addr') || request.headers.get('True-Client-IP') || request.headers.get('X-Real-Ip') || request.headers.get('X-Real-Ip');
-
-  return new Response(ip, {
-    headers: { 'content-type': 'text/plain' },
-  });
-}
