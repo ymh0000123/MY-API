974a962eb6db13c5a6b3c02c73491f485c4f1210
ymh0000123 <ymh0000123@163.com>
2024-06-25 18:35:07 +0800

    Revert "升级"
    This reverts commit cfe97f153fa045c161ba3bae4171d591d1fc2e19.

--- functions/api/ip.js
+++ functions/api/ip.js
@@ -1,23 +1,22 @@
 export async function onRequest(context) {
-  // 获取请求的客户端 IP 地址
-  const clientIP = context.request.headers.get('cf-connecting-ip') || 'IP Not Found';
+    // 获取请求的客户端 IP 地址
+    const clientIP = context.request.headers.get('cf-connecting-ip') || 'IP Not Found';
+    
+    // 获取请求的地理位置信息
+    const { city, region, country } = context.request.cf || {};
   
-  // 获取请求的地理位置信息，包括城市、地区、国家和邮政编码
-  const { city, region, country, postalCode } = context.request.cf || {};
-
-  // 创建包含 IP 地址、地理位置信息和邮政编码的 JSON 响应对象
-  const jsonResponse = JSON.stringify({
-    ip: clientIP,
-    location: {
-      city: city || 'Unknown',
-      region: region || 'Unknown',
-      country: country || 'Unknown',
-      postalCode: postalCode || 'Unknown'
-    }
-  });
-
-  // 返回 JSON 响应
-  return new Response(jsonResponse, {
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
+    // 创建包含 IP 地址和地理位置信息的 JSON 响应对象
+    const jsonResponse = JSON.stringify({
+      ip: clientIP,
+      location: {
+        city: city || 'Unknown',
+        region: region || 'Unknown',
+        country: country || 'Unknown',
+      }
+    });
+  
+    // 返回 JSON 响应
+    return new Response(jsonResponse, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
