4c4f246a2a2f436425cdefbffff73d636a73f6fc
ymh0000123 <ymh0000123@163.com>
2024-06-25 14:22:27 +0800

    1

--- functions/api/ip.js
+++ functions/api/ip.js
@@ -1,8 +1,11 @@
-export async function handler(event, context) {
-  const ip = event.request.headers.get('cf-connecting-ip'); // Cloudflare specific header
-  return new Response(ip || 'Unable to determine IP address', {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const ip = request.headers.get('CF-Connecting-IP') || request.headers.get('X-Forwarded-For') || request.headers.get('x-real-ip') || request.headers.get('Remote_Addr') || request.headers.get('True-Client-IP') || request.headers.get('X-Real-Ip') || request.headers.get('X-Real-Ip');
+
+  return new Response(ip, {
+    headers: { 'content-type': 'text/plain' },
   });
 }
