f1005c06e02d36f13c63c326e4e52d8dad7c992f
ymh0000123 <ymh0000123@163.com>
2024-06-26 13:34:17 +0800

    hyp

--- functions/api/hyp.js
+++
@@ -1,8 +0,0 @@
-export default async function(context){
-    if  (url.searchParams.get('id') != null){
-        return new Response("Hello, world!")
-    }
-    else if (url.searchParams.get('id') == null){
-        return new Response("no")
-    }
-}
