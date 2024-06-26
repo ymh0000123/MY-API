ecce36df388ef997b1586a0e8514687f8061573c
ymh0000123 <ymh0000123@163.com>
2024-06-26 20:42:52 +0800

    hyp

--- functions/api/hyp.js
+++ functions/api/hyp.js
@@ -4,9 +4,9 @@ export async function onRequest(context) {
     const id = url.searchParams.get('uuid');
 
     if (id) {
-        const response = await fetch(`https://api.hypixel.net/v2/playerr?key=${context.env.HF_API_KEY}&uuid=${id}`);
+        const response = await fetch(`https://api.hypixel.net/v2/playerr?uuid=${id}`);
         const data = await response.json();
-        return new Response(JSON.stringify(data), { status: 200 });
+        return new Response(JSON.stringify(data), { status: 200 },{headers: { 'Content-Type': 'application/json' ,'API-Key': context.env.HF_API_KEY},});
     } else {
         return new Response('No UUID provided', { status: 400 });
     }
