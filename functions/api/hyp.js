96a4322401bf41af71f6c44c397077b080ac2408
ymh0000123 <ymh0000123@163.com>
2024-06-26 20:23:30 +0800

    hyp

--- functions/api/hyp.js
+++ functions/api/hyp.js
@@ -4,7 +4,7 @@ export async function onRequest(context) {
     const id = url.searchParams.get('uuid');
 
     if (uuid) {
-        const response = await fetch(`https://api.hypixel.net/player?key=${ontext.env.HYPIXEL_KEY}&uuid=${uuid}`);
+        const response = await fetch(`https://api.hypixel.net/player?key=${context.env.HF_API_KEY}&uuid=${uuid}`);
         const data = await response.json();
         return new Response(JSON.stringify(data), { status: 200 });
     } else {
